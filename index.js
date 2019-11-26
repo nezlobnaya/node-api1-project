a91884cf699e5916b4f176549b1da732addd7080
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-11-26 18:50:07 -0500

    Added a GET api endpoint

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const port = 5000
 
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send('Hello World!')
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+      res.status(200).json({ 
+        users })
+    })
+    .catch(error => {
+      res.status(500, 'The users information could not be retrieved', res)
+    })
 })
 
 server.listen(port, () => 
