7e31b49e5eb0606cff71532333eeb5f051a6e99d
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-11-26 17:52:45 -0500

    Installed express and launched the server on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// implement your API here
+const express = require('express')
+const db = require('./data/db')
+
+const port = 5000
+
+const server = express()
+
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+server.listen(port, () => 
+    console.log(`server is listening on port ${port}`))
+
